44e97d97fb1d6c1ae98de0a31b39d84b11424e8f
ishita-gupta <56958134+ishita-gupta@users.noreply.github.com>
2019-11-17 11:59:08 +0530

    Add files via upload

---
+++ solar system/js/weight.js
@@ -0,0 +1,2 @@
+function compute_weight(t){var u=t.wt.value;u>0&&(t.outputmrc.value=int_zero(10*u*.378)/10,t.outputvn.value=int_zero(10*u*.907)/10,t.outputmars.value=int_zero(10*u*.377)/10,t.outputjp.value=int_zero(10*u*2.528)/10,t.outputsat.value=int_zero(10*u*1.064)/10,t.outputur.value=int_zero(10*u*.889)/10,t.outputnpt.value=int_zero(10*u*1.125)/10)}
+function int_zero(t){return 1>t?0:parseInt(t,10)}// JavaScript Document
